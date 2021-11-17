78ddbe88f8771c3a0982c66e31f5645eaa1a7c44
GAAdel <32236445+GAAdel@users.noreply.github.com>
2021-11-17 21:52:54 +0300

    database

--- education_redbean2.sql
+++ education_redbean2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Ноя 17 2021 г., 17:57
+-- Время создания: Ноя 17 2021 г., 19:52
 -- Версия сервера: 10.3.16-MariaDB
 -- Версия PHP: 7.3.6
 
@@ -31,38 +31,31 @@ SET time_zone = "+00:00";
 CREATE TABLE `answers` (
   `id` int(11) NOT NULL,
   `age` varchar(255) DEFAULT NULL,
-  `isLicei` varchar(255) DEFAULT NULL,
+  `is_licei` varchar(255) DEFAULT NULL,
   `stepen` varchar(255) DEFAULT NULL,
-  `isColledge` varchar(255) DEFAULT NULL,
+  `is_colledge` varchar(255) DEFAULT NULL,
   `gender` varchar(255) DEFAULT NULL,
-  `forma1` varchar(255) DEFAULT NULL,
+  `forma` varchar(255) DEFAULT NULL,
   `sovet` varchar(255) DEFAULT NULL,
   `forma2` varchar(255) DEFAULT NULL,
   `subjects` varchar(255) DEFAULT NULL,
   `vstypit` varchar(255) DEFAULT NULL,
   `naprav` varchar(255) DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
-  `isHome` varchar(255) DEFAULT NULL,
-  `isActive` varchar(255) DEFAULT NULL,
-  `isLgoti` varchar(255) DEFAULT NULL,
-  `isBudget` varchar(255) DEFAULT NULL,
+  `is_home` varchar(255) DEFAULT NULL,
+  `is_active` varchar(255) DEFAULT NULL,
+  `is_lgoti` varchar(255) DEFAULT NULL,
+  `is_budget` varchar(255) DEFAULT NULL,
   `budget` varchar(255) DEFAULT NULL,
-  `ball` varchar(255) DEFAULT NULL,
-  `is_licei` varchar(191) DEFAULT NULL,
-  `is_home` varchar(191) DEFAULT NULL,
-  `is_active` varchar(191) DEFAULT NULL,
-  `is_lgoti` varchar(191) DEFAULT NULL,
-  `is_budget` varchar(191) DEFAULT NULL,
-  `is_colledge` varchar(191) DEFAULT NULL
+  `ball` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `answers`
 --
 
-INSERT INTO `answers` (`id`, `age`, `isLicei`, `stepen`, `isColledge`, `gender`, `forma1`, `sovet`, `forma2`, `subjects`, `vstypit`, `naprav`, `city`, `isHome`, `isActive`, `isLgoti`, `isBudget`, `budget`, `ball`, `is_licei`, `is_home`, `is_active`, `is_lgoti`, `is_budget`, `is_colledge`) VALUES
-(1, '18', NULL, 'Специалитет', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(2, '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `answers` (`id`, `age`, `is_licei`, `stepen`, `is_colledge`, `gender`, `forma`, `sovet`, `forma2`, `subjects`, `vstypit`, `naprav`, `city`, `is_home`, `is_active`, `is_lgoti`, `is_budget`, `budget`, `ball`) VALUES
+(1, '119', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -88,21 +81,21 @@ CREATE TABLE `questions` (
 
 INSERT INTO `questions` (`id`, `text`, `type`, `answer1`, `answer2`, `answer3`, `answer4`, `parametr`, `id_rule`) VALUES
 (1, 'Укажите ваш возраст?', 0, NULL, NULL, NULL, NULL, 'age', 1),
-(2, 'Вас интересует начальное профессиональное образование (училище, лицей)?', 2, 'Да', 'Нет', NULL, NULL, 'isLicei', 2),
+(2, 'Вас интересует начальное профессиональное образование (училище, лицей)?', 2, 'Да', 'Нет', NULL, NULL, 'is_licei', 2),
 (3, 'Какая степень обучения вас интересует?', 4, 'Бакалавриат', 'Специалитет', 'Магистратура', 'Аспирантура', 'stepen', 3),
-(4, 'Вас интересует среднее профессиональное образование (колледж, техникум) ?', 2, 'Колледж', 'Техникум', NULL, NULL, 'isColledge', 4),
+(4, 'Вас интересует среднее профессиональное образование (колледж, техникум) ?', 2, 'Колледж', 'Техникум', NULL, NULL, 'is_colledge', 4),
 (5, 'Укажите ваш пол?', 2, 'Муж', 'Жен', NULL, NULL, 'gender', 5),
 (6, 'Какая форма обучения вам подходит?', 3, 'Очная', 'Заочная', 'Очно-заочная', NULL, 'forma', 6),
 (7, 'Наличие диссертационного совета?', 2, 'Да', 'Нет', NULL, NULL, 'sovet', 10),
-(8, 'Какая форма обучения вам подходит?', 3, 'Очная', 'Заочная', 'Очно-Заочная', NULL, 'forma', 11),
+(8, 'Какая форма обучения вам подходит?', 3, 'Очная', 'Заочная', 'Очно-Заочная', NULL, 'forma2', 11),
 (9, 'Предметы поступления?', 4, 'Информатика/Математика', 'Физика/Математика', 'Биология/Химия', 'Обществознание/История', 'subjects', 11),
 (10, 'Вступительные испытания', 2, 'Да', 'Нет', NULL, NULL, 'vstypit', 11),
 (11, 'Какое направление обучения?', 4, 'Информатика', 'Медицина', 'Строительство', 'Журналистика', 'naprav', 12),
 (12, 'В каком городе хотите учиться?', 3, 'Москва', 'Санкт-Петербург', 'Казань', NULL, 'city', 13),
-(13, 'Нужно ли вам место проживания?', 2, 'Да', 'Нет', NULL, NULL, 'isHome', 14),
-(14, 'Наличие активной внеучебной деятельности?', 2, 'Да', 'Нет', NULL, NULL, 'isActive', 15),
-(15, 'Имеются ли льготы?', 2, 'Да', 'Нет', NULL, NULL, 'isLgoti', 16),
-(16, 'Рассматриваете только бюджет?', 2, 'Да', 'Нет', NULL, NULL, 'isBudget', 17),
+(13, 'Нужно ли вам место проживания?', 2, 'Да', 'Нет', NULL, NULL, 'is_home', 14),
+(14, 'Наличие активной внеучебной деятельности?', 2, 'Да', 'Нет', NULL, NULL, 'is_active', 15),
+(15, 'Имеются ли льготы?', 2, 'Да', 'Нет', NULL, NULL, 'is_lgoti', 16),
+(16, 'Рассматриваете только бюджет?', 2, 'Да', 'Нет', NULL, NULL, 'is_budget', 17),
 (17, 'Бюджет от ?', 0, NULL, NULL, NULL, NULL, 'budget', 777),
 (18, 'Средний балл?', 0, NULL, NULL, NULL, NULL, 'ball', 777);
 
@@ -172,7 +165,7 @@ ALTER TABLE `rules`
 -- AUTO_INCREMENT для таблицы `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT для таблицы `questions`
