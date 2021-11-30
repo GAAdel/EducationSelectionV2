f3896e8b9113e2715a334d6f79a7982f9112dae8
GAAdel <32236445+GAAdel@users.noreply.github.com>
2021-11-30 22:25:33 +0300

    dumping

--- output.php
+++ output.php
@@ -4,6 +4,19 @@ require_once "blocks/header.php";
 ?>
 <?php
 
+function dump1($elem)
+{
+  echo '</pre>';
+  var_dump($elem);
+  echo '</pre>';
+}
+
+function dump2($elem, $text = '')
+{
+  echo $text . " " . $elem;
+  echo '</br>';
+}
+
 function isEmpty($elem)
 {
   if ($elem == "") {
@@ -13,107 +26,50 @@ function isEmpty($elem)
   }
 }
 
+
 $answers = R::load('answers', 1);
 
+for ()
+
+$rules = R::load('simple_rules', 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// foreach ($answers as $answer){
+//   echo $answer . '<br>';
+// }
+// dump2($answers->age);
+// dump2($answers->is_licei);
+// dump2($answers->stepen);
+// dump2($answers->is_colledge);
+// dump2($answers->gender);
+// dump2($answers->forma);
+// dump2($answers->sovet);
+// dump2($answers->forma2);
+// dump2($answers->subjects);
+// dump2($answers->vstypit);
+// dump2($answers->naprav);
+// dump2($answers->city);
+// dump2($answers->is_home);
+// dump2($answers->is_active);
+// dump2($answers->is_lgoti);
+// dump2($answers->is_budget);
+// dump2($answers->budget);
+// dump2($answers->ball);
 
 ?>
 
-  <main class="main container">
-    <form action="index.php" method="get">
-
-      <?php if (isEmpty($answers->age)) { ?>
-        <label for="">Ваш возраст:</label>
-        <input type=" text" name="" value="<?php echo $answers->age?>" /><br>
-      <?php } ?>
-
-      <?php if (isEmpty($answers->is_licei)) { ?>
-        <label for="">Вам нужен лицей:</label>
-        <input type=" text" name="" value="<?php echo $answers->is_licei?>" /><br>
-      <?php } ?>
-
-      <?php if (isEmpty($answers->stepen)) { ?>
-        <label for="">Выбранная степень:</label>
-        <input type=" text" name="" value="<?php echo $answers->stepen?>" /><br>
-      <?php } ?>
-
-      <?php if (isEmpty($answers->is_colledge)) { ?>
-        <label for="">Вы выбрали:</label>
-        <input type=" text" name="" value="<?php echo $answers->is_colledge?>" /><br>
-      <?php } ?>
-
-      <?php if (isEmpty($answers->gender)) { ?>
-        <label for="">Ваш пол:</label>
-        <input type=" text" name="" value="<?php echo $answers->gender?>" /><br>
-      <?php } ?>
-      
-      <?php if (isEmpty($answers->forma)) { ?>
-        <label for="">Форма обучения:</label>
-        <input type=" text" name="" value="<?php echo $answers->forma?>" /><br>
-      <?php } ?>
-      
-      <?php if (isEmpty($answers->sovet)) { ?>
-        <label for="">Наличие диссертационного совета:</label>
-        <input type=" text" name="" value="<?php echo $answers->sovet?>" /><br>
-      <?php } ?>
-
-      <?php if (isEmpty($answers->forma2)) { ?>
-        <label for="">Форма обучения:</label>
-        <input type=" text" name="" value="<?php echo $answers->forma2?>" /><br>
-      <?php } ?>
-
-      <?php if (isEmpty($answers->subjects)) { ?>
-        <label for="">Выбранные предметы:</label>
-        <input type=" text" name="" value="<?php echo $answers->subjects?>" /><br>
-      <?php } ?>
-      
-      <?php if (isEmpty($answers->vstypit)) { ?>
-        <label for="">Наличие вступительных испытаний:</label>
-        <input type=" text" name="" value="<?php echo $answers->vstypit?>" /><br>
-      <?php } ?>
-
-      <?php if (isEmpty($answers->naprav)) { ?>
-        <label for="">Направление обучения:</label>
-        <input type=" text" name="" value="<?php echo $answers->naprav?>" /><br>
-      <?php } ?>
-
-      <?php if (isEmpty($answers->city)) { ?>
-        <label for="">Город поступления:</label>
-        <input type=" text" name="" value="<?php echo $answers->city?>" /><br>
-      <?php } ?>
-
-      <?php if (isEmpty($answers->is_home)) { ?>
-        <label for="">Необходимо место проживания:</label>
-        <input type=" text" name="" value="<?php echo $answers->is_home?>" /><br>
-      <?php } ?>
-      
-      <?php if (isEmpty($answers->is_active)) { ?>
-        <label for="">Активная внеучебная деятельность:</label>
-        <input type=" text" name="" value="<?php echo $answers->is_active?>" /><br>
-      <?php } ?>
-
-      <?php if (isEmpty($answers->is_lgoti)) { ?>
-        <label for="">Наличие льгот:</label>
-        <input type=" text" name="" value="<?php echo $answers->is_lgoti?>" /><br>
-      <?php } ?>
-      
-      <?php //if (isEmpty($answers->is_budget)) { ?>
-        <!-- <label for="">Рассматриваете бюджет:</label>
-        <input type=" text" name="" value="<?php echo $answers->is_budget?>" /><br> -->
-      <?php //} ?>
-
-      <?php if (isEmpty($answers->budget)) { ?>
-        <label for="">Ваш бюджет:</label>
-        <input type=" text" name="" value="<?php echo $answers->budget?>" /><br>
-      <?php } ?>
-      
-      <?php if (isEmpty($answers->ball)) { ?>
-        <label for="">Ваш средний балл:</label>
-        <input type=" text" name="" value="<?php echo $answers->ball?>" /><br>
-      <?php } ?>
-
-      <input class="btn" type="submit" value="Заново" />
-    </form>
-  </main>
   
 <?php
 require "blocks/footer.php";
